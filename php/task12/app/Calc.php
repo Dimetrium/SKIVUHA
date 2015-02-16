aa674c935252f43925da25da717fd7d4283a1119
skivuha <skivuha@hotmail.com>
2015-02-17 01:04:06 +0200

    task 12 in work

--- php/task12/app/Calc.php
+++ php/task12/app/Calc.php
@@ -3,6 +3,7 @@ class Calc
 {
   public $a;
   public $b;
+  protected $err;
   
 
   public function __construct()
@@ -11,30 +12,91 @@ class Calc
 
   public function setA($a)
   {
-    return $this->a = $a;
+    if(!is_numeric($a))
+    {
+      $this->a=$a;
+      return false;
+    }
+    elseif(trim($a)=='')
+      unset($a);
+    else
+      $this->a= $a;
+  }
+
+  public function getA()
+  {
+    if(!is_numeric($this->a))
+    { 
+      $this->err = 'your value is wrong data!';
+      return false;
+    }
+    elseif(isset($this->a))
+    return $this->a;
+    else
+    {
+      $this->err = 'your value is empty';
+      return false;
+    }
   }
 
   public function setB($b)
   {
-    return $this->b = $b;
-  }    
+    if(!is_numeric($b))
+    {
+      $this->b=$b;
+      return false;
+    }
+    elseif(trim($b)=='')
+      unset($b);
+    else
+      $this->b = $b;
+  }
+
+  public function getB()
+  { 
+    if(!is_numeric($this->b))
+    {
+      $this->err = 'your value is wrong data!';
+      return false;
+    }
+    elseif(isset($this->b))
+    return $this->b;
+    else
+    {
+      $this->err = 'your value is empty';
+      return false;
+    }
+  }
 
   public function sum()
   {
     $sum = $this->a + $this->b;
-    return $sum; 
+    return $sum;
   }
 
   public function sub()
   {
+    $sub = $this->a - $this->b;
+    return $sub;
   }
 
   public function mul()
   {
+    $mul = $this->a * $this->b;
+    return $mul;
   }
 
   public function div()
   {
+    if($this->b == 0)
+    {
+      $this->err = 'na nol delit nelzya';
+      return false;
+    }
+    else 
+      $sub = $this->a / $this->b;
+     return true;
+     
   }
 }
 ?>
--- php/task12/app/CalcTest.php
+++ php/task12/app/CalcTest.php
@@ -3,10 +3,123 @@ include("Calc.php");
 class CalcTest extends PHPunit_Framework_TestCase
 
 {
-  public function testSum()
+  protected $calc;
+  protected $sum;
+  protected $sub;
+  protected $div;
+  protected $mul;
+  
+  function setUp()
   {
-    $calc = new Calc();
-    $expected = $calc->a+$calc->b; 
-    $this->assertEquals($expected, $calc->sum());
+    $this->calc=new Calc();
+    $this->sum = $this->calc->a+$this->calc->b;
+    $this->sub = $this->calc->a-$this->calc->b;
+    $this->mul = $this->calc->a*$this->calc->b;
+
+  }
+
+  function tearDown()
+  {
+    $this->calc=NULL;
+    $this->sub=NULL;
+    $this->sum=NULL;
+    $this->div=NULL;
+    $this->mul=NULL;
+  }
+
+  public function testSumConditionEquals()
+  {
+    $this->assertEquals($this->sum, $this->calc->sum());
+  }
+
+  public function testSumConditionTrue()
+  {
+    $this->assertTrue($this->sum == $this->calc->sum());
+  }
+  
+  public function testSumConditionFalse()
+  {
+    $this->assertFalse($this->sum != $this->calc->sum());
+  }
+
+  public function testSubConditionEquals()
+  {
+    $this->assertEquals($this->sub, $this->calc->sub());
+  }
+  
+  public function testSubConditionTrue()
+  {
+    $this->assertTrue($this->sub == $this->calc->sub());
+  }
+
+  public function testSubConditionFalse()
+  {
+    $this->assertFalse($this->sub != $this->calc->sub());
+  }
+
+  public function testMulConditionEquals()
+  {
+    $this->assertEquals($this->mul, $this->calc->mul());
+  }
+
+  public function testMulConditionEqualsTrue()
+  {
+    $this->assertTrue($this->mul == $this->calc->mul());
+  }
+  public function testMulConditionEqualsFalse()
+  {
+    $this->assertFalse($this->mul != $this->calc->mul());
+  }
+  
+  public function testSetAnotEmpty()
+  {
+    $this->calc->setA('');
+    $this->assertFalse($this->calc->getA());
+  
+    $this->calc->setA(5);
+    $this->assertTrue(null !== $this->calc->getA());
   }  
+ 
+  public function testSetAnotArray()
+  {
+    $this->calc->setA(array(5));
+    $this->assertFalse($this->calc->getA());
+  }
+
+  public function testSetBnotEmpty()
+  {
+    $this->calc->setB('');
+    $this->assertFalse($this->calc->getB());
+  
+    $this->calc->setB(5);
+    $this->assertTrue(null !== $this->calc->getB());
+  }  
+ 
+  public function testSetBnotArray()
+  {
+    $this->calc->setB(array(5));
+    $this->assertFalse($this->calc->getB());
+  }
+  
+  public function testSetBnotString()
+  {
+    $this->calc->setB('mimimi');
+    $this->assertFalse($this->calc->getB());
+  }
+
+
+  public function testSetAnotString()
+  {
+    $this->calc->setA('mimimi');
+    $this->assertFalse($this->calc->getA());
+  }
+
+  public function testDivConditionDivByZero()
+  {
+    
+    $this->calc->setA(1);
+    $this->calc->setB(0);
+    $this->assertFalse($this->calc->div());
+  }
 }
+?>
