a1a6f06d35faa2a4953d9f9f27dab49f32a7ca89
Student 2 <user2@tc.geeksforless.net>
2015-03-12 09:47:06 +0200

    angular

--- js/angular/app/js/controllers.js
+++ js/angular/app/js/controllers.js
@@ -1,5 +1,16 @@
 var app = angular.module('shop',[]);
 
+/*app.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise("/home");
+	
+	$stateProvider
+	.state('home', {
+      url: "/",
+      templateUrl: "partials/main.html"
+    });
+});
+*/
+
 app.factory('book', function($http) {
     return {
         getBook: function(callback) {
@@ -8,11 +19,12 @@ app.factory('book', function($http) {
     };
 });
 
-app.controller('shopController', function($scope, book){
-    book.getBook(function(rez){
-             //this.book = rez;
-        $scope.books = rez.book;
-        console.log(rez.book);
+app.controller('shopController', function(book){
+	var _this = this;
+    book.getBook(function(rez){ 
+        _this.books = rez;
+		return _this.books;   
     })
-    
+	
+
  });
